65f0c111f840f93de32d0269c4adeef93aad387f
liyanzhuo <654024083@qq.com>
2024-09-27 20:51:59 +0800

    finish collatz 1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,16 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** A method that returns the next number of
+     * the input number in a collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0){
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
+
+
     }
 
     public static void main(String[] args) {
