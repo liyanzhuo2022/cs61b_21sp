7edca05929899f93a20a928d19e62daf8275bbcd
liyanzhuo <654024083@qq.com>
2025-04-07 16:33:21 +0200

    try debug 36a)

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -599,10 +599,9 @@ public class Repository {
             String givenID = givenMap.getOrDefault(fileName, notExist);
 
             // try to debug 36a): Skip restoring files that were deleted in current after a prior merge
-            if (splitID.equals(givenID) && !splitID.equals(notExist) && curID.equals(notExist)) {
+            if (!splitID.equals(notExist) && splitID.equals(givenID) && curID.equals(notExist)) {
                 continue;
             }
-
 
             if (!splitID.equals(curID) && !splitID.equals(givenID)
                     && !curID.equals(givenID)) {
